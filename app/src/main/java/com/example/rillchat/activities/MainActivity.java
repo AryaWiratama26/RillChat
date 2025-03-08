06d85ab662affa34be995991fe4ca05665857540
farelaryaduta <farelminecraft450@gmail.com>
2025-03-08 12:02:27 +0700

    Sign Up linked to Firebase

--- app/src/main/java/com/example/rillchat/activities/MainActivity.java
+++ app/src/main/java/com/example/rillchat/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.rillchat;
+package com.example.rillchat.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.rillchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
