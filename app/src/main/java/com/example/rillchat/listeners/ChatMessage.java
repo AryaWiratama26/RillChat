4ed7315908fc33099164a61d86d20db5b235cae4
Arya Wiratama <aryawiratama2401@gmail.com>
2025-03-13 18:25:54 +0700

    belum selesai 8

---
+++ app/src/main/java/com/example/rillchat/listeners/ChatMessage.java
@@ -0,0 +1,7 @@
+package com.infinityandroid.rillchat.models;
+
+public class ChatMessage {
+    public String senderId, receiverId, message, dateTime;
+
+    
+}
---
+++ app/src/main/java/com/example/rillchat/listeners/UserListener.java
@@ -0,0 +1,5 @@
+package com.infinityandroid.rillchat.listeners;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
